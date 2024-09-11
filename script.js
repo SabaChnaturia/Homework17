ea82ae01cd5f1fcd87082d4b0e08f4c3d094a896
SabaChnaturia <154993776+SabaChnaturia@users.noreply.github.com>
2024-09-11 18:04:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function comparison(firstStr,SecStr) {
+    if (firstStr.length === SecStr.length){
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
+}
+console.log(comparison("saba", "tako"));
